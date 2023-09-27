bf66df29ec302dd0cce6404d48da31627bbf2ffa
Torben Kreuder <torben.kreuder@gmail.com>
2023-09-27 20:58:30 +0200

    Fix sqlalchemy conflict

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ install_requires = [
     "tiktoken",
     "dataclasses_json",
     "langchain>=0.0.293",
-    "sqlalchemy>=2.0.15",
+    "sqlalchemy>=2.0.11",
     "numpy",
     "tenacity>=8.2.0,<9.0.0",
     "openai>=0.26.4",
